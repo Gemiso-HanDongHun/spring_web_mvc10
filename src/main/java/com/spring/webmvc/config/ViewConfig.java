515fe48c39dfc1513bccecee1afcb3749e216e1a
LUYCVN <dh5777895@naver.com>
2022-10-27 09:41:39 +0900

    add

---
+++ src/main/java/com/spring/webmvc/config/ViewConfig.java
@@ -0,0 +1,21 @@
+package com.spring.webmvc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class ViewConfig {
+
+//    @Bean
+//    public InternalResourceViewResolver resolver() {
+//        InternalResourceViewResolver resolver
+//                = new InternalResourceViewResolver();
+//        resolver.setPrefix("/WEB-INF/views/");
+//        resolver.setSuffix(".jsp");
+//
+//        return resolver;
+//    }
+
+
+}
